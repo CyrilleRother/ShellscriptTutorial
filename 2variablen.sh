19f7688ed722019921821339f84d11c28545533c
CyrilleRother <103304715+CyrilleRother@users.noreply.github.com>
2022-07-09 09:50:30 +0200

    Use Variables
    Use Variables in Linux Shellscript

---
+++ 2variablen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+variable1='Hallo der Text wird durch aufrufen einer Variable ausgegeben'
+echo $variable1
+
+variable2=1234567890
+echo $variable2
+
+variable3=AllesEinWort
+echo $variable3
