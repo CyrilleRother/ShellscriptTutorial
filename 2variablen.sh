e7aa16682f3e54915af15a85c8363927cfb9bd56
CyrilleRother <103304715+CyrilleRother@users.noreply.github.com>
2022-07-09 09:55:30 +0200

    Variablen benutzen
    Einfaches Script um Variablen zu benutzen

--- 2variablen.sh
+++ 2variablen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-variable1='Hallo der Text wird durch aufrufen einer Variable ausgegeben'
+variable1='Hallo der Text wird durch aufrufen einer Variable ausgegeben!'
 echo $variable1
 
 variable2=1234567890
